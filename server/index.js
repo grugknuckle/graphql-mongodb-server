893774d7a7b3dd53ced4fa86d0f9287d94bc0430
Grugknuckle <awol@bete.com>
2019-02-13 16:52:07 -0500

    added better home route.

--- server/index.js
+++ server/index.js
@@ -6,6 +6,9 @@ const graphql = require('./database/graphql.js')
 const models = database(process.env.mongoURI)
 const server = graphql(models)
 
+const dev = !(process.env.NODE_ENV === 'production')
+const host = process.env.HOST || '127.0.0.1'
+
 const options = {
   port: process.env.PORT || '4000',
   endpoint: '/graphql',
@@ -13,6 +16,13 @@ const options = {
   playground: '/playground'
 }
 
+server.get('/', (req, res, next) => {
+  res.json({
+    message: 'This is the root directory of adawg\'s famous quotation API. In production, this route will redirect you to the front end for the project.',
+    playground: ''
+  })
+})
+
 server.start(options, ({ port }) => {
-  console.log(`GraphQL server is running on http://localhost:${port}`)
+  console.log(`GraphQL server is running on ${dev ? 'http' : 'https' }://${ host }:${ port }`)
 })
