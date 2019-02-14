74386d411c516e06b1211394f71944a7548c31b6
Grugknuckle <awol@bete.com>
2019-02-14 10:39:03 -0500

    added some comments in the server/index.js file.

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const graphql = require('./database/graphql.js')
 const models = database(process.env.mongoURI)
 const server = graphql(models)
 
+// import config variables
 const dev = !(process.env.MODE === 'production')
 const host = process.env.HOST || '127.0.0.1'
 const port = process.env.PORT || '4000'
@@ -18,6 +19,8 @@ const options = {
   playground: '/playground'
 }
 
+// define home route as a standard REST endpoint.
+// NOTE: Once a front end application is deployed, this endpoint should redirect to the home route of that app.
 server.get('/', (req, res, next) => {
   res.json({
     message: 'This is the root directory of adawg\'s famous quotation API. In production, this route will redirect you to the front end for the project.',
@@ -27,6 +30,23 @@ server.get('/', (req, res, next) => {
   })
 })
 
+
+// Start the server.
 server.start(options, ({ port }) => {
   console.log(`GraphQL server is running on ${ domain }`)
 })
+
+/*
+  NOTES:
+  The graphql-yoga library (which makes the server) is based on express.js. 
+
+  GraphQLServer exposes the express.Application directly via its express property:
+
+    server.express.use(myMiddleware())
+
+  Middleware can also be added specifically to the GraphQL endpoint route, by using:
+
+    server.express.post(server.options.endpoint, myMiddleware())
+  
+  Any middleware you add to that route, will be added right before the apollo-server-express middleware.
+*/
